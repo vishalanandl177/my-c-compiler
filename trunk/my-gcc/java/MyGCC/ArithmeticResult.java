10953f525bb654f6dbf8586c1ce06cf3bbb706c9
elyas.bhy <elyas.bhy@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-03-19 15:52:00 +0000

    Enabled generation of arithmetic expressions

--- trunk/my-gcc/java/MyGCC/ArithmeticResult.java
+++ trunk/my-gcc/java/MyGCC/ArithmeticResult.java
@@ -3,10 +3,12 @@ package MyGCC;
 public class ArithmeticResult extends ParsingResult{
 
     public ResultType myType;
+
     public String left;
     public String op;
     public String right;
     public String value;
+
 
     public ArithmeticResult(String l, String op, String r, String val){
         super(ResultType.ARITHMETIC);
@@ -23,4 +25,28 @@ public class ArithmeticResult extends ParsingResult{
     public String getValue(){
         return this.value;
     }
- }
+
+    public void setLeft(String expr){
+	this.left = expr;
+    }
+
+    public String getLeft(){
+	return this.left;
+    }
+
+    public void setRight(String expr){
+	this.right = expr;
+    }
+
+    public String getRight(){
+	return this.right;
+    }
+
+    public void setOp(String expr){
+	this.op = expr;
+    }
+
+    public String getOp(){
+	return this.op;
+    }
+}
