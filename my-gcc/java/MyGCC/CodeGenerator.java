783d2c6f9db47b3aa3a11adb98601e140583ff84
medrimonia <medrimonia@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-03-19 15:56:01 +0000

    Beginning of Work on functions

--- my-gcc/java/MyGCC/CodeGenerator.java
+++ my-gcc/java/MyGCC/CodeGenerator.java
@@ -6,25 +6,45 @@ import java.io.*;
 
 public class CodeGenerator{
 
-  private LinkedList<Stack<ParsingResult>> myStack;
+  private LinkedList<Stack<Object>> myStack;
 
-  private Context globalcontext;
+  private Context globalContext;
+  private Context actualContext;
 
   public CodeGenerator(){
-    myStack = new LinkedList<Stack<ParsingResult>>();
+    myStack = new LinkedList<Stack<Object>>();
   }
 
-  public void pushInformation(ParsingResult p){
+  public void pushInformation(Object o){
     if (myStack.size() == 0)
       openNewContext();
-    myStack.getLast().push(p);
+    myStack.getLast().push(o);
   }
 
   public void openNewContext(){
-    myStack.add(new Stack<ParsingResult>());
+    myStack.add(new Stack<Object>());
   }
 
-  public void declareFunction(){
+  public void declarePrototype(){
+    Type returnType = null;
+    String name = null;
+    LinkedList<Type> parameters = null;
+
+    Stack<Object> tmpStack = myStack.getLast();
+    while (!tmpStack.isEmpty()){
+      Object o = tmpStack.pop();
+      if (o instanceof Type)
+        returnType = (Type) o;
+      else if (o instanceof String)
+        name = (String) o;
+      else if (o instanceof LinkedList)
+        parameters = (LinkedList<Type>) o;
+    }
+    
+    System.out.println("Declaring a prototype with :"
+                       + "\nReturnType : " + returnType
+                       + "\nname : " + name);
+    
     
   }
 
--- my-gcc/tests/test1.c
+++ my-gcc/tests/test1.c
@@ -1,4 +1,6 @@
 int addition(int,int);
+int testing(int);
+int basic(void);
 
 int addition(int a,int b){
   int c;
