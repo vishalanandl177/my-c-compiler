936b13f7efea5c2033a44f94fb241ce5d54ec942
rya.herb <rya.herb@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-03-21 10:48:07 +0000

    Began implementing Declaration class

--- my-gcc/java/MyGCC/Declaration.java
+++ my-gcc/java/MyGCC/Declaration.java
@@ -1,5 +1,27 @@
 package MyGCC;
 
 public class Declaration {
-  //TODO
+  
+  public Type type;
+  public String identifier;
+  public int arraySize;
+  
+  public Declaration(Type t, String id, int arraySize) {
+    this.type = t;
+    this.identifier = id;
+    this.arraySize = arraySize;
+  }
+  
+  public Type getType() {
+    return this.type;
+  }
+  
+  public String getIdentifier() {
+    return this.identifier;
+  }
+  
+  public int getArraySize() {
+    return this.arraySize;
+  }  
+  
 }
