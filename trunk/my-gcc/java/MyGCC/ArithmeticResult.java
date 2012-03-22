edb2bac157a6b413372d7f8d48d86ef1bfd1b6f4
elyas.bhy <elyas.bhy@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-03-22 15:04:07 +0000

    Added abstract class Expression in order to set up tree management. Modified ArithmeticResult.java

--- trunk/my-gcc/java/MyGCC/ArithmeticResult.java
+++ trunk/my-gcc/java/MyGCC/ArithmeticResult.java
@@ -1,52 +1,22 @@
 package MyGCC;
 
-public class ArithmeticResult extends ParsingResult{
+public class ArithmeticResult extends Expression{
 
     public ResultType myType;
 
-    public String left;
-    public String op;
-    public String right;
-    public String value;
 
-
-    public ArithmeticResult(String l, String op, String r, String val){
-        super(ResultType.ARITHMETIC);
-        this.left = l;
-        this.op = op;
-        this.right = r;
-        this.value = val;
+    public ArithmeticResult(String value, String op){
+      super(ResultType.ARITHMETIC);
+      this.op = op;
+      this.value = value;
+    }
+    
+    public ArithmeticResult(String value, String op, Expression l, Expression r){
+      super(ResultType.ARITHMETIC);
+      this.op = op;
+      this.value = value;
+      this.left = l;
+      this.right = r;
     }
 
-    public void setValue(String expr){
-        this.value = expr;
-    }
-
-    public String getValue(){
-        return this.value;
-    }
-
-    public void setLeft(String expr){
-	this.left = expr;
-    }
-
-    public String getLeft(){
-	return this.left;
-    }
-
-    public void setRight(String expr){
-	this.right = expr;
-    }
-
-    public String getRight(){
-	return this.right;
-    }
-
-    public void setOp(String expr){
-	this.op = expr;
-    }
-
-    public String getOp(){
-	return this.op;
-    }
 }
