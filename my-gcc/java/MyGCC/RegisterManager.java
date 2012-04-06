3d82b561ddc774747b03c8659b941bdbd46f9aec
rya.herb <rya.herb@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-04-06 11:00:12 +0000

    Added enum for Error management, changed code in Register Manager and Register

--- my-gcc/java/MyGCC/RegisterManager.java
+++ my-gcc/java/MyGCC/RegisterManager.java
@@ -1,6 +1,7 @@
 package MyGCC;
 
 import java.util.*;
+import java.util.AbstractMap.*;
 
 public class RegisterManager {
   private HashMap<Register, String> usedRegisters;
@@ -21,26 +22,62 @@ public class RegisterManager {
   }
   
   public Register getFreeRegister() {
-    HashSet<Register> list = Register.getCallerSaved();
-    Iterator<Register> iter;
-    iter = list.iterator();
-    Register r;
     
-    while(iter.hasNext()) {
-      r = iter.next();
-      if(!this.usedRegisters.containsKey(r))
-        return r;
+    ArrayList<HashSet<Register>> al = new ArrayList<HashSet<Register>>(); 
+    HashSet<Register> list = Register.getCallerSaved();
+    al.add(list);
+    list = Register.getCalleeSaved();
+    al.add(list);
+    
+    Register r;
+    Iterator<Register> iter;
+    
+    for(HashSet<Register> hsr : al) {
+      iter = hsr.iterator();
+      
+      while(iter.hasNext()) {
+        r = iter.next();
+        if(!this.usedRegisters.containsKey(r))
+          return r;
+      }
     }
 
-    list = Register.getCalleeSaved();
-    iter = list.iterator();
-    while(iter.hasNext()) {
-      r = iter.next();
-      if(!this.usedRegisters.containsKey(r))
-        return r;
-    }
-    
     System.err.println("No free registers were found.");
+    return null;
+  }
+  
+  public SimpleEntry<Register, Register> getTwoRegisters() {
+    
+    ArrayList<HashSet<Register>> al = new ArrayList<HashSet<Register>>();   
+    HashSet<Register> list = Register.getCallerSaved();
+    al.add(list);
+    list = Register.getCalleeSaved();
+    al.add(list);
+
+    
+    Iterator<Register> iter;
+    Register r = null;
+    Register r1 = null;
+    Register r2 = null;
+    
+    for(HashSet<Register> hsr : al) {
+      iter = hsr.iterator();
+    
+      while(iter.hasNext()) {
+        r = iter.next();
+        if(!this.usedRegisters.containsKey(r))
+          if(r1 == null)
+            r1 = r;
+          else
+            r2 = r;
+      }
+    
+      if(r1 != null && r2 != null)
+        return (new SimpleEntry<Register, Register>(r1, r2));
+    }
+
+    System.out.println("Assigning two registers failed");
+    
     return null;
   }
   
@@ -74,5 +111,11 @@ public class RegisterManager {
           this.usedRegisters.remove(reg);
     else
       System.err.println("The variable was not set to any registers.");
+  }
+  
+  public static void main(String[] args) {
+    RegisterManager rm = new RegisterManager();
+    Register r = rm.getFreeRegister();
+    System.out.println(r.toString());    
   }
 }
