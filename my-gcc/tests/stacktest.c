fa0b31857461fae00d3da1fec11462486cdf9f32
medrimonia <medrimonia@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-04-06 11:03:04 +0000

    Little modifications on functions and some tests about stack added

---
+++ my-gcc/tests/stacktest.c
@@ -0,0 +1,8 @@
+int teststack(int a, int b, int c){
+  int d,e;
+  a = a;
+  b = b;
+  c = c;
+  d = d + d;
+  e = e + e;  
+}
---
+++ my-gcc/tests/stacktest2.c
@@ -0,0 +1,7 @@
+int teststack(int a, int b){
+  int d,e;
+  a = a;
+  b = b;
+  d = d + d;
+  e = e + e;  
+}
