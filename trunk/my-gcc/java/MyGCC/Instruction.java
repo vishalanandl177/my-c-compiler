a56a6f42d823491dccce26b13fb249259bb902fa
elyas.bhy <elyas.bhy@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-04-13 08:52:07 +0000

    Adjusted instruction generation

--- trunk/my-gcc/java/MyGCC/Instruction.java
+++ trunk/my-gcc/java/MyGCC/Instruction.java
@@ -2,12 +2,21 @@ package MyGCC;
 
 public class Instruction {
   
-  public Expression expr;
+  public Expression lexpr;
+  public Expression rexpr;
   public InstructionType instrType;
   public String assemblyStr;
   
-  public Instruction(Expression e, InstructionType op){
-    this.expr = e;
+  public Instruction(Expression r, InstructionType op){
+    this.lexpr = null;
+    this.rexpr = r;
+    this.instrType = op;
+    this.assemblyStr = null;
+  }
+  
+  public Instruction(Expression l, Expression r, InstructionType op){
+    this.lexpr = l;
+    this.rexpr = r;
     this.instrType = op;
     this.assemblyStr = null;
   }
@@ -47,20 +56,19 @@ public class Instruction {
     String center = new String();
     Register regL = null;
     Register regR = null; 
-    Register reg = null;
     ArithmeticResult tmpL = null;
     ArithmeticResult tmpR = null;  
     ArithmeticResult tmp = null;  
      
     if(instruct != null){
-      if(instruct.expr == null){
+      if(instruct.rexpr == null){
         //sb.append("\tleave\n\tret\n"); Already handled in epilogue()
       }
       
-      if(instruct.expr instanceof ArithmeticResult){
-        tmpL = (ArithmeticResult)instruct.expr.left;
-        tmpR = (ArithmeticResult)instruct.expr.right;
-        tmp = (ArithmeticResult)instruct.expr;
+      if(instruct.rexpr instanceof ArithmeticResult){
+        tmpL = (ArithmeticResult)instruct.rexpr.left;
+        tmpR = (ArithmeticResult)instruct.rexpr.right;
+        tmp = (ArithmeticResult)instruct.lexpr;
         center = String.valueOf(tmp.getValue());
         left = String.valueOf(tmpL.getValue());
         right = String.valueOf(tmpR.getValue());
@@ -73,40 +81,40 @@ public class Instruction {
       switch(instruct.instrType){
         
         case RETURN:
-          sb.append("\tmovl " /*+ instr.expr.value*/ + ", %eax");
+          sb.append("\tmovl " /*+ instr.rexpr.value*/ + ", %eax");
           break;
           
         case EXIT:
           //TODO
-          break;  
+          break;
           
         case EQL:
           if(tmpL.getValue() instanceof Integer && tmpR.getValue() instanceof Integer){
-            reg = Parser.regMan.addVariableToRegister(center); //FIXME: center is null
-            System.out.println("I-I INSTRUCTION:\n" + center);
-            sb.append(handleIntInt((Integer)tmpL.getValue(), (Integer)tmpR.getValue(), instruct.expr.op, reg));
+            regR = Parser.regMan.addVariableToRegister(center, 'e');
+            sb.append(handleIntInt((Integer)tmpL.getValue(), (Integer)tmpR.getValue(), instruct.rexpr.op, regR));
           }
         
           else if(tmpL.getValue() instanceof String && tmpR.getValue() instanceof String){
-              regL = Parser.regMan.addVariableToRegister(left);
-              regR = Parser.regMan.addVariableToRegister(right);
+              regL = Parser.regMan.addVariableToRegister(left, 'e');
+              regR = Parser.regMan.addVariableToRegister(right, 'e');
               sb.append("\tmovl %" + tmpL.getValue() + ", %" + regL.toString() + "\n");
               sb.append("\tmovl %" + tmpR.getValue() + ", %" + regR.toString() + "\n");
-              sb.append("\t" + instruct.expr.op.toString() + " %" + regL.toString() + ", %" + regR.toString() + "\n");
-              System.out.println("S-S INSTRUCTION:\n" + sb.toString());
+              sb.append("\t" + instruct.rexpr.op.toString() + " %" + regL.toString() + ", %" + regR.toString() + "\n");
           }
             
           else if(tmpL.getValue() instanceof Integer && tmpR.getValue() instanceof String){
-              regR = Parser.regMan.addVariableToRegister(right);
-              sb.append("\t" + instruct.expr.op.toString() + " %" + tmpL.getValue() + ", %" + regR.toString() + "\n");
-              System.out.println("I-S INSTRUCTION:\n" + sb.toString());
+              regR = Parser.regMan.addVariableToRegister(right, 'e');
+              sb.append("\tmovl %" + tmpR.getValue() + ", %" + regR.toString() + "\n");
+              sb.append("\t" + instruct.rexpr.op.toString() + " %" + tmpL.getValue() + ", %" + regR.toString() + "\n");
           }
             
           else if(tmpL.getValue() instanceof String && tmpR.getValue() instanceof Integer){
-              regL = Parser.regMan.addVariableToRegister(left);
-              sb.append("\t" + instruct.expr.op.toString() + " %" + tmpR.getValue() + ", %" + regL.toString() + "\n");
-              System.out.println("S-I INSTRUCTION:\n" + sb.toString());
+              regR = Parser.regMan.addVariableToRegister(left, 'e');
+              sb.append("\tmovl %" + tmpL.getValue() + ", %" + regR.toString() + "\n");
+              sb.append("\t" + instruct.rexpr.op.toString() + " %" + tmpR.getValue() + ", %" + regR.toString() + "\n");
           }
+          
+          sb.append("\tmovl %" + regR.toString() + ", -x(%rbp)" + "\n");
           
           break;
           
