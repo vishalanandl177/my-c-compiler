f95e989847332624cf2acf083709048de4c076bf
rya.herb@gmail.com <rya.herb@gmail.com@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-05-02 10:08:45 +0000

    forgot to add the LogicalifElse class

---
+++ trunk/my-gcc/java/MyGCC/LogicalIfElse.java
@@ -0,0 +1,48 @@
+package MyGCC;
+
+import java.util.*;
+
+public class LogicalIfElse extends LogicalBlock {
+  
+  public Block elseBlock = null;
+  
+  public LogicalIfElse(Expression r, InstructionType op) {
+    super(r, op);
+  }
+  
+  public LogicalIfElse(Expression r, InstructionType op, Block b, Block elseBlock) {
+    super(r, op, b);
+    this.elseBlock = elseBlock;
+  }
+  
+  public static String instructionToAssembly(LogicalIfElse instruct, Context context) throws Exception {
+		StringBuffer sb = new StringBuffer();
+    
+    if(instruct != null){
+      if(instruct.rexpr == null){
+        System.out.println("rexpr is null");
+        // do nothing: already handled in epilogue()
+        return sb.toString();
+      }
+      System.out.println("LogicalIfElse");
+      //System.out.println("\tInstruction type: " + instruct.type.toString());
+      String label1 = LabelManager.getLabel();
+      sb.append('\t'); sb.append(instruct.rexpr.handleExpression(null, context)); sb.append('\n');
+      sb.append('\t'); sb.append(instruct.rexpr.op.getName()); sb.append(" "); sb.append(label1); sb.append('\n');
+      sb.append(instruct.block.toString());
+       
+      if(instruct.elseBlock != null) {
+        String label2 = LabelManager.getLabel();
+        sb.append('\t'); sb.append("jmp "); sb.append(label2); sb.append('\n');
+        sb.append('.'); sb.append(label1); sb.append(":\n");
+        sb.append(instruct.elseBlock.toString());
+        sb.append('.'); sb.append(label2); sb.append(":\n");
+      }
+      System.out.println("Finished assembling LogicalIfElse");
+      return sb.toString();
+    }
+    
+    System.err.println("The instruction is null");
+    return null;
+  }
+}
