852673b615a59eb848f665afb470a070f7a2ba78
rya.herb <rya.herb@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-05-03 15:44:43 +0000

    Class used by Context for variable management

---
+++ trunk/my-gcc/java/MyGCC/ContextEntry.java
@@ -0,0 +1,11 @@
+package MyGCC;
+
+public class ContextEntry {
+  public Type type;
+  public int arraySize;
+  
+  public ContextEntry(Type t, int as) {
+    this. type = t;
+    this.arraySize  = as;
+  }
+}
