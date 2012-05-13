cbbc8ec00a7a256c267738baaa147081412a6a92
medrimonia <medrimonia@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-05-13 04:17:18 +0000

    obsolete files deleted

--- trunk/my-gcc/tests/stacktest.c
+++
@@ -1,8 +0,0 @@
-int teststack(int a, int b, int c){
-  int d,e;
-  a = a;
-  b = b;
-  c = c;
-  d = d + d;
-  e = e + e;  
-}
--- trunk/my-gcc/tests/stacktest2.c
+++
@@ -1,7 +0,0 @@
-int teststack(int a, int b){
-  int d,e;
-  a = a;
-  b = b;
-  d = d + d;
-  e = e + e;  
-}
