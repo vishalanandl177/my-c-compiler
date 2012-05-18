bdc3f4dfdecba7f3ca6353230dd2ed49b81d0683
rya.herb@gmail.com <rya.herb@gmail.com@91afa725-b60f-fa47-bcd7-923c2e2bc272>
2012-05-18 15:14:08 +0000

    added class for Static management, not fully  implemented

--- trunk/my-gcc/tests/singular_tests/test_array.c
+++ trunk/my-gcc/tests/singular_tests/test_array.c
@@ -1,10 +1,27 @@
-int test_array(int);
+int test_array(void);
+int total_sum(int[3]);
 
-int test_array(int i){
-  int c[3];
-  c[0] = 0;
-  c[1] = 1;
-  c[2] = 2;
-  
-  return c[i%3];
+
+int total_sum(int tab[3]){
+  int sum;
+  int index;
+  sum = 0;
+  index = 0;
+  while(index < 3){
+		sum = sum + tab[index];
+		index = index + 1;
+	};
+  return sum;
+}
+
+int test_array(void){
+  int a;
+  int t[3];
+  a = 2;
+  t[0] = 8;
+  t[1] = 9;
+  t[2] = 10;
+
+  //return total_sum(t);
+  return t[a%3];
 }
